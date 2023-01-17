99d0ba81ccc9d8baa095584cdf911c891d44abc2
Muanya <nwokejichimuanya@gmail.com>
2023-01-17 23:30:01 +0100

    basic layout completed

---
+++ README.md
@@ -0,0 +1,4 @@
+# image_capture
+
+An app for capturing image
+
---
+++ lib/gallery_view.dart
@@ -0,0 +1,113 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+
+
+class GalleryPhotoViewWrapper extends StatefulWidget {
+  GalleryPhotoViewWrapper({Key? key,
+    required this.backgroundDecoration,
+    this.minScale,
+    this.maxScale,
+    required this.initialIndex,
+    required this.photoItems,
+    this.scrollDirection = Axis.horizontal,
+  }) : pageController = PageController(initialPage: initialIndex), super(key: key);
+
+  final BoxDecoration backgroundDecoration;
+  final dynamic minScale;
+  final dynamic maxScale;
+  final int initialIndex;
+  final PageController pageController;
+  final List<String> photoItems;
+  final Axis scrollDirection;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _GalleryPhotoViewWrapperState();
+  }
+}
+
+class _GalleryPhotoViewWrapperState extends State<GalleryPhotoViewWrapper> {
+  late int currentIndex;
+
+  @override
+  void initState() {
+    currentIndex = widget.initialIndex;
+    super.initState();
+  }
+
+  void onPageChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: widget.backgroundDecoration,
+        constraints: BoxConstraints.expand(
+          height: MediaQuery.of(context).size.height,
+        ),
+        child: Stack(
+          alignment: Alignment.bottomRight,
+          children: <Widget>[
+            PhotoViewGallery.builder(
+              builder: _buildPhoto,
+              scrollPhysics: const BouncingScrollPhysics(),
+              itemCount: widget.photoItems.length,
+              backgroundDecoration: widget.backgroundDecoration,
+              pageController: widget.pageController,
+              onPageChanged: onPageChanged,
+              scrollDirection: widget.scrollDirection,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                File(widget.photoItems[currentIndex]).path.split('/').last,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 17.0,
+                  decoration: null,
+                ),
+              ),
+            ),
+            SafeArea(
+              child: Container(
+                alignment: Alignment.topLeft,
+                margin: const EdgeInsets.only(top: 16),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const BackButton(),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  PhotoViewGalleryPageOptions _buildPhoto(BuildContext context, int index) {
+    final String item = widget.photoItems[index];
+    return PhotoViewGalleryPageOptions.customChild(
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child:  Image.file(
+          File(item),
+          height: 200.0,
+        ),
+      ),
+      childSize: const Size(300, 300),
+      initialScale: PhotoViewComputedScale.contained,
+      minScale: PhotoViewComputedScale.contained * (0.5 + index / 10),
+      maxScale: PhotoViewComputedScale.covered * 1.1,
+      heroAttributes: PhotoViewHeroAttributes(tag: index),
+    );
+
+  }
+}
